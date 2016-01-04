1a43b1cc52a5bb556cfc523e6c44870a73eb7be5
大塚広道 <hiro_o2o2a@ezweb.ne.jp>
2016-01-04 12:17:53 +0900

    first commit

--- 付箋ランチャー/ViewController.h
+++ 付箋ランチャー/ViewController.h
@@ -10,4 +10,23 @@
 
 @interface ViewController : UIViewController
 
+//tabVIewのOutlet
+@property (weak, nonatomic) IBOutlet UIView *tab1View;
+@property (weak, nonatomic) IBOutlet UIView *tab2View;
+@property (weak, nonatomic) IBOutlet UIView *tab3View;
+@property (weak, nonatomic) IBOutlet UIView *tab4View;
+@property (weak, nonatomic) IBOutlet UIButton *tab1btn;
+@property (weak, nonatomic) IBOutlet UIButton *tab2btn;
+@property (weak, nonatomic) IBOutlet UIButton *tab3btn;
+@property (weak, nonatomic) IBOutlet UIButton *tab4btn;
+@property (weak, nonatomic) IBOutlet UIView *menuView;
+@property (weak, nonatomic) IBOutlet UIButton *menuBtn;
+
+//tabView操作用のアクション
+- (IBAction)menu_btn:(id)sender;
+- (IBAction)main_tab1:(id)sender;
+- (IBAction)main_tab2:(id)sender;
+- (IBAction)main_tab3:(id)sender;
+- (IBAction)main_tab4:(id)sender;
+
 @end
--- 付箋ランチャー/ViewController.m
+++ 付箋ランチャー/ViewController.m
@@ -8,16 +8,26 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    BOOL isMenuOpen;
+}
 
 @end
 
 @implementation ViewController
+@synthesize tab1View;
+@synthesize tab2View;
+@synthesize tab3View;
+@synthesize tab4View;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    isMenuOpen = NO;
+}
+
+-(void)viewWillAppear:(BOOL)animated{
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +36,150 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)menu_btn:(id)sender {
+    if(isMenuOpen){
+        //メニューを閉じる操作
+        [UIView
+         animateWithDuration:0.2
+         animations:^{
+             _menuView.frame = CGRectOffset(_menuView.frame, +79, 0);
+         }
+         ];
+        [UIView
+         animateWithDuration:0.2
+         animations:^{
+             _menuBtn.frame = CGRectOffset(_menuBtn.frame, +79, 0);
+         }
+         ];
+
+        isMenuOpen = NO;
+    }else{
+        //メニューを開く操作
+        [UIView
+         animateWithDuration:0.2
+         animations:^{
+             _menuView.frame = CGRectOffset(_menuView.frame, -79, 0);
+         }
+         ];
+        [UIView
+         animateWithDuration:0.2
+         animations:^{
+             _menuBtn.frame = CGRectOffset(_menuBtn.frame, -79, 0);
+         }
+         ];
+        isMenuOpen = YES;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//tabView操作用のアクション
+- (IBAction)main_tab1:(id)sender {
+    [self.view bringSubviewToFront:tab4View];
+    [self.view bringSubviewToFront:_tab4btn];
+    [self.view bringSubviewToFront:tab3View];
+    [self.view bringSubviewToFront:_tab3btn];
+    [self.view bringSubviewToFront:tab2View];
+    [self.view bringSubviewToFront:_tab2btn];
+    [self.view bringSubviewToFront:tab1View];
+    [self.view bringSubviewToFront:_tab1btn];
+    [self.view bringSubviewToFront:_menuBtn];
+    [self.view bringSubviewToFront:_menuView];
+
+    isMenuOpen = NO;
+
+    
+}
+
+- (IBAction)main_tab2:(id)sender {
+    [self.view bringSubviewToFront:tab4View];
+    [self.view bringSubviewToFront:_tab4btn];
+    [self.view bringSubviewToFront:tab3View];
+    [self.view bringSubviewToFront:_tab3btn];
+    [self.view bringSubviewToFront:tab1View];
+    [self.view bringSubviewToFront:_tab1btn];
+    [self.view bringSubviewToFront:tab2View];
+    [self.view bringSubviewToFront:_tab2btn];
+    [self.view bringSubviewToFront:_menuBtn];
+    [self.view bringSubviewToFront:_menuView];
+
+    isMenuOpen = NO;
+
+}
+
+- (IBAction)main_tab3:(id)sender {
+    [self.view bringSubviewToFront:tab4View];
+    [self.view bringSubviewToFront:_tab4btn];
+    [self.view bringSubviewToFront:tab2View];
+    [self.view bringSubviewToFront:_tab2btn];
+    [self.view bringSubviewToFront:tab1View];
+    [self.view bringSubviewToFront:_tab1btn];
+    [self.view bringSubviewToFront:tab3View];
+    [self.view bringSubviewToFront:_tab3btn];
+    [self.view bringSubviewToFront:_menuBtn];
+    [self.view bringSubviewToFront:_menuView];
+
+    isMenuOpen = NO;
+
+    
+}
+
+- (IBAction)main_tab4:(id)sender {
+    [self.view bringSubviewToFront:tab3View];
+    [self.view bringSubviewToFront:_tab3btn];
+    [self.view bringSubviewToFront:tab2View];
+    [self.view bringSubviewToFront:_tab2btn];
+    [self.view bringSubviewToFront:tab1View];
+    [self.view bringSubviewToFront:_tab1btn];
+    [self.view bringSubviewToFront:tab4View];
+    [self.view bringSubviewToFront:_tab4btn];
+    [self.view bringSubviewToFront:_menuBtn];
+    [self.view bringSubviewToFront:_menuView];
+
+    isMenuOpen = NO;
+
+}
 @end
